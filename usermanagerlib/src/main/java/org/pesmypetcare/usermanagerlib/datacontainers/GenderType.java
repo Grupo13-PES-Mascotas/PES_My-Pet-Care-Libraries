2cdadd53a08c8361bd310cc9622504cd0525c78a
Santiago Del Rey Juárez <santiago.del.rey@est.fib.upc.edu>
2020-03-26 01:43:10 +0100

    Add missing classes

---
+++ usermanagerlib/src/main/java/org/pesmypetcare/usermanagerlib/datacontainers/GenderType.java
@@ -0,0 +1,7 @@
+package org.pesmypetcare.usermanagerlib.datacontainers;
+
+public enum GenderType {
+    Male,
+    Female,
+    Other
+}
---
+++ usermanagerlib/src/main/java/org/pesmypetcare/usermanagerlib/datacontainers/PetData.java
@@ -0,0 +1,85 @@
+package org.pesmypetcare.usermanagerlib.datacontainers;
+
+import java.util.Date;
+
+public class PetData {
+    private GenderType gender;
+    private String breed;
+    private Date birth;
+    private Double weight;
+    private String pathologies;
+    private Double recommendedKcal;
+    private int washFreq;
+
+    public PetData() {
+    }
+
+    public GenderType getGender() {
+        return gender;
+    }
+
+    public void setGender(GenderType gender) {
+        this.gender = gender;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public String getPathologies() {
+        return pathologies;
+    }
+
+    public void setPathologies(String pathologies) {
+        this.pathologies = pathologies;
+    }
+
+    public Double getRecommendedKcal() {
+        return recommendedKcal;
+    }
+
+    public void setRecommendedKcal(Double recommendedKcal) {
+        this.recommendedKcal = recommendedKcal;
+    }
+
+    public int getWashFreq() {
+        return washFreq;
+    }
+
+    public void setWashFreq(int washFreq) {
+        this.washFreq = washFreq;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            "gender=" + gender +
+            ", breed='" + breed + '\'' +
+            ", birth=" + birth +
+            ", weight=" + weight +
+            ", pathologies='" + pathologies + '\'' +
+            ", recommendedKcal=" + recommendedKcal +
+            ", washFreq=" + washFreq +
+            '}';
+    }
+}
