8c744c0df5ea1c7fb68540afa2951f2d45d92c06
oricat8 <oriol.catalan.fernandez@est.fib.upc.edu>
2020-06-03 17:19:51 +0200

    Codacy

--- usermanager/src/main/java/org/pesmypetcare/usermanager/datacontainers/user/UserMedalData.java
+++ usermanager/src/main/java/org/pesmypetcare/usermanager/datacontainers/user/UserMedalData.java
@@ -2,7 +2,6 @@ package org.pesmypetcare.usermanager.datacontainers.user;
 
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 import java.util.Arrays;
 import java.util.List;
